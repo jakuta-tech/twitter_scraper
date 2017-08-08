c999fe8e5ba0aeb5d353b58cd60eb714aaf47b31
jfantell <jfantell@users.noreply.github.com>
2017-08-07 23:44:58 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 // config/database.js
 module.exports = {
 
-    //'url' : 'mongodb://localhost:27017/tweets_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    //'url' : 'mongodb://fantej:webscigroup8@ds123351.mlab.com:23351/db_test'
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
